406e8c81a59cdef701527d5fd46719511790b82e
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-08-11 20:05:49 +0200

    update build

--- build.sh
+++ build.sh
@@ -2,20 +2,26 @@ LALSUITE_VERSION=6.48
 NUMPY_VERSION=1.19
 
 mkdir local
+export PATH=$PATH:/opt/python/cp27-cp27m/bin/
+
+yum install -y zlib-devel
+
+# Install numpy
+pip install numpy=='1.13.0'
 
 # Install libframe
 curl http://lappweb.in2p3.fr/virgo/FrameL/libframe-8.30.tar.gz > libframe.tar.gz
 tar -xf libframe.tar.gz
-cd librame*
-./configure --prefix=../local
+cd libframe-*
+./configure --prefix=$PWD/../local
 make -j install
 cd ../
 
 # Install libmetaio
 curl http://software.ligo.org/lscsoft/source/metaio-8.4.0.tar.gz > metaio.tar.gz
 tar -xf metaio.tar.gz
-cd metaio*
-./configure --prefix=../local
+cd metaio-*
+./configure --prefix=$PWD/../loca
 make -j install
 cd ../
 
@@ -26,7 +32,7 @@ cd lalsuite*
 
 ./00boot
 ./configure \
---prefix=../local/ \
+--prefix=$PWD/../loca \
 --enable-swig-python \
 --disable-lalapps \
 --disable-lalinference \
