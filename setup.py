1b10d4e460178e5e1c13c1a1c75628b140df1ea5
Alexander Harvey Nitz <alex.nitz@ligo.org>
2018-01-26 20:56:54 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ class bdist_wheel(_bdist_wheel):
         self.root_is_pure = False
 
 setup(name='lal',
-      version='0.0.5',
+      version='0.0.6',
       description='LIGO Algorithm Library for Python',
       author='Alex Nitz',
       author_email='alex.nitz@aei.mpg.de',
